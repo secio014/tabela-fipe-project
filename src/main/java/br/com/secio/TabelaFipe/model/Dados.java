d89c8b595124dfeb4b9f3ef5185b78c2369c9618
Pedro Schmitz Sécio <pedro.secio014@gmail.com>
2025-09-02 16:54:58 -0300

    feat: add model folder and vehicles record

---
+++ src/main/java/br/com/secio/TabelaFipe/model/Dados.java
@@ -0,0 +1,4 @@
+package br.com.secio.TabelaFipe.model;
+
+public record Dados(String codigo, String nome) {
+}
---
+++ src/main/java/br/com/secio/TabelaFipe/model/Veiculo.java
@@ -0,0 +1,19 @@
+package br.com.secio.TabelaFipe.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true);
+public record Veiculo(
+        @JsonAlias("Valor") String valor,
+        @JsonAlias("Marca") String marca,
+        @JsonAlias("Modelo") String modelo,
+        @JsonAlias("AnoModelo") Integer ano,
+        @JsonAlias("Combustivel") String tipoCombustivel
+) {
+    @Override
+    public String toString() {
+        return String.format("%s %s  ano: %s valor: %s combustível: %s",
+                marca, modelo, ano, valor, tipoCombustivel);
+    }
+}
